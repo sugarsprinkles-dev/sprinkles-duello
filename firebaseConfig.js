0c284c846cfb695ad732da048d2463b3e9b59498
sugarsprinkles-dev <goksudenizkartal@gmail.com>
2025-04-21 14:07:33 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,9 @@
-// Firebase yapılandırmanızı buraya ekleyin
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6LqgFE94WuesPDPlZf8y8zZ92tH8EG6o",
+  authDomain: "sprinkles-duello.firebaseapp.com",
+  databaseURL: "https://sprinkles-duello-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sprinkles-duello",
+  storageBucket: "sprinkles-duello.firebasestorage.app",
+  messagingSenderId: "684570040506",
+  appId: "1:684570040506:web:acd1ac22849b43c0528a83"
+};
